b1d0523ed30628479f02de1af20e3f6856e4d1db
Harikrishna Lodhi <hklodhi2904@gmail.com>
2025-11-15 16:25:52 +0530

    Added The-Latest section

--- store.js
+++ store.js
@@ -30,3 +30,39 @@ nextBtn.addEventListener('click', () => {
 backBtn.addEventListener('click', () => {
   categoryScroll.scrollLeft -= 100;
 });
+
+// Latest Items Scroll bar
+
+let latestScroll = document.querySelector('.latest-items');
+let latestBackBtn = document.getElementById('latest-back-btn');
+let latestNextBtn = document.getElementById('latest-next-btn');
+
+let updateLatestBtnVisibility = () => {
+  let latestMaxScroll = latestScroll.scrollWidth - latestScroll.clientWidth;
+
+  if(latestScroll.scrollLeft <= 200){
+    latestBackBtn.classList.add('hidden');
+  }
+  else if(latestScroll.scrollLeft >= latestMaxScroll-160){
+    latestNextBtn.classList.add('hidden');
+  }
+  else{
+    latestBackBtn.classList.remove('hidden');
+    latestNextBtn.classList.remove('hidden');
+  }
+}
+
+updateButtonVisibility();
+
+requestAnimationFrame(()=>{
+  latestBackBtn.classList.remove('no-transition')
+})
+
+latestScroll.addEventListener('scroll',updateLatestBtnVisibility);
+
+latestNextBtn.addEventListener('click', () => {
+  latestScroll.scrollLeft += 420;
+});
+latestBackBtn.addEventListener('click', () => {
+  latestScroll.scrollLeft -= 420;
+});
