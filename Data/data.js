dfc2b68b01eb37028b9160636f9f8441e0078f04
Harikrishna Lodhi <hklodhi2904@gmail.com>
2025-11-20 19:02:18 +0530

    Data file Added

---
+++ Data/data.js
@@ -0,0 +1,100 @@
+const latestProducts = [{
+  textColor: 'white',
+  itemImg: 'Images/store-card-iphone-17-pro.jpg',
+  itemName: 'iPhone 17 Pro',
+  itemTagline: 'All out Pro.',
+  itemPrice: '134900.00'
+}, {
+  textColor: 'white',
+  itemImg: 'Images/store-card-macbook-pro.jpg',
+  itemName: 'MacBook Pro 14"',
+  itemTagline: 'Supercharged by M5.',
+  itemPrice: '169900.00'
+}, {
+  textColor: '',
+  itemImg: 'Images/store-card-iphone-air.jpg',
+  itemName: 'iPhone Air',
+  itemTagline: 'The thinnest iPhone ever.',
+  itemPrice: '119900.00'
+}, {
+  textColor: 'white',
+  itemImg: 'Images/store-card-ipad-pro.jpg',
+  itemName: 'iPad Pro',
+  itemTagline: 'Mmmmm. Power.',
+  itemPrice: '99900.00'
+}, {
+  textColor: '',
+  itemImg: 'Images/store-card-iphone-17.jpg',
+  itemName: 'iPhone 17',
+  itemTagline: 'Magichromatic.',
+  itemPrice: '82900.00'
+}, {
+  textColor: '',
+  itemImg: 'Images/store-card-watch-s.jpg',
+  itemName: 'Apple Watch Series 11',
+  itemTagline: 'The ultimate way to watch your health.',
+  itemPrice: '46900.00'
+}, {
+  textColor: '',
+  itemImg: 'Images/store-card-watch-se.jpg',
+  itemName: 'Apple Watch SE 3',
+  itemTagline: 'Walk it. Talk it. Track it. Love it.',
+  itemPrice: '25900.00'
+}, {
+  textColor: 'white',
+  itemImg: 'Images/store-card-watch-ultra.jpg',
+  itemName: 'Apple Watch Ultra 3',
+  itemTagline: 'Personal beast.',
+  itemPrice: '89900.00'
+}, {
+  textColor: '',
+  itemImg: 'Images/store-card-airpods-pro.jpg',
+  itemName: 'AirPods Pro 3',
+  itemTagline: `The World's best in-ear.`,
+  itemPrice: '25900.00'
+}];
+
+
+const HelpSection = [{
+  img:'Images/shop-video-call.jpg',
+  head:'Shop with a Specialist over video.',
+  subHead:'Choose your next device in a guded, one-way video session.',
+  tag:'',
+  textColor:''
+}, {
+  img:'Images/apple-specialist.jpg',
+  head:'Shop one on one with a Specialist online.',
+  subHead:'',
+  tag:'APPLE SPECIALIST',
+  textColor:''
+}, {
+  img:'Images/today-at-apple.jpg',
+  head:'Explore Apple Intelligence',
+  subHead:'Come and try it for yourself in a free session <br> at the Apple Store.',
+  tag:'TODAY AT APPLE',
+  textColor:''
+}, {
+  img:'Images/free-session.jpg',
+  head:'Join free sessions at your <br>Apple Store.',
+  subHead:'Learn about the latest features and how to go further <br>with your Apple devices.',
+  tag:'TODAY AT APPLE',
+  textColor:''
+}, {
+  img:'Images/small-buisness.jpg',
+  head:`From enterprise to small business, we'll work with you.<sup>1</sup>`,
+  subHead:'',
+  tag:'SMALL BUSINESS',
+  textColor:'white'
+}, {
+  img:'Images/personal-setup.jpg',
+  head:`Set up your new device with help from a Specialist.`,
+  subHead:'Let us guide you through data transfer, the latest <br> features and more, in an online, one-to-one session.',
+  tag:'PERSONAL SETUP',
+  textColor:''
+}, {
+  img:'Images/watch-and-learn.jpg',
+  head:`Need support? Our teas are <br> here for you.`,
+  subHead:'',
+  tag:'WATCH AND LEARN',
+  textColor:''
+}];
