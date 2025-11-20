adb38f2fca9e6ca52a65c84ed40d97cee48c966a
Harikrishna Lodhi <hklodhi2904@gmail.com>
2025-11-20 19:03:45 +0530

    Added Help section

--- store.js
+++ store.js
@@ -25,10 +25,10 @@ requestAnimationFrame(()=>{
 categoryScroll.addEventListener('scroll',updateButtonVisibility);
 
 nextBtn.addEventListener('click', () => {
-  categoryScroll.scrollLeft += 100;
+  categoryScroll.scrollLeft += 251;
 });
 backBtn.addEventListener('click', () => {
-  categoryScroll.scrollLeft -= 100;
+  categoryScroll.scrollLeft -= 251;
 });
 
 // Latest Items Scroll bar
@@ -43,7 +43,7 @@ let updateLatestBtnVisibility = () => {
   if(latestScroll.scrollLeft <= 200){
     latestBackBtn.classList.add('hidden');
   }
-  else if(latestScroll.scrollLeft >= latestMaxScroll-160){
+  else if(latestScroll.scrollLeft >= latestMaxScroll-165){
     latestNextBtn.classList.add('hidden');
   }
   else{
@@ -61,8 +61,79 @@ requestAnimationFrame(()=>{
 latestScroll.addEventListener('scroll',updateLatestBtnVisibility);
 
 latestNextBtn.addEventListener('click', () => {
-  latestScroll.scrollLeft += 420;
+  latestScroll.scrollLeft += 251;
 });
 latestBackBtn.addEventListener('click', () => {
-  latestScroll.scrollLeft -= 420;
+  latestScroll.scrollLeft -= 251;
 });
+
+// Help Scroll bar
+
+let helpScroll = document.querySelector('.help-section');
+let helpBackBtn = document.getElementById('help-back-btn');
+let helpNextBtn = document.getElementById('help-next-btn');
+
+let updateHelpBtnVisibility = () => {
+  let helpMaxScroll = helpScroll.scrollWidth - helpScroll.clientWidth;
+
+  if(helpScroll.scrollLeft <= 250){
+    helpBackBtn.classList.add('hidden');
+  }
+  else if(helpScroll.scrollLeft >= helpMaxScroll-60){
+    helpNextBtn.classList.add('hidden');
+  }
+  else{
+    helpBackBtn.classList.remove('hidden');
+    helpNextBtn.classList.remove('hidden');
+  }
+}
+
+updateHelpBtnVisibility();
+
+requestAnimationFrame(()=>{
+  helpBackBtn.classList.remove('no-transition')
+})
+
+helpScroll.addEventListener('scroll',updateHelpBtnVisibility);
+
+helpNextBtn.addEventListener('click', () => {
+  helpScroll.scrollLeft += 251;
+});
+helpBackBtn.addEventListener('click', () => {
+  helpScroll.scrollLeft -= 251;
+});
+
+// Latest Items Data
+
+let latestHTML = '';
+
+latestProducts.forEach((latest)=>{
+  latestHTML += `
+    <div>
+      <div class="${latest.textColor}" style="background-image:url(${latest.itemImg});">
+        <div>${latest.itemName}</div>
+        <div>${latest.itemTagline}</div>
+        <div>From &#8377;${latest.itemPrice}<sup>‡</sup></div>
+        <div class="last-child">Add to Bag</div>
+      </div>
+    </div>
+  `;
+});
+document.querySelector('.latest-items').innerHTML = latestHTML;
+
+// Help Section Data
+
+let helpHTML = '';
+
+HelpSection.forEach((help)=>{
+  helpHTML += `
+    <div>
+        <div class="${help.textColor}" style="background-image:url(${help.img});">
+          <div>${help.head}</div>
+          <div>${help.subHead}</div>
+          <div class="ctg-tag">${help.tag}</div>
+        </div>
+      </div>
+  `;
+});
+document.querySelector('.help-section').innerHTML = helpHTML;
